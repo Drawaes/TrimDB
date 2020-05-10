cd1fa3bf1432743ec8b3888d2c6b053cd50a03ae
Tim <seawardtim@gmail.com>
2020-05-10 12:39:52 +0100

    Added another hash to the benchmarks

--- benchmarks/TrimDB.Benchmarks/Hash.cs
+++ benchmarks/TrimDB.Benchmarks/Hash.cs
@@ -11,35 +11,42 @@ namespace TrimDB.Benchmarks
     public class Hash
     {
         public byte[] _data;
-        private static Random rand = new Random(7722);
+        private static readonly Random _rand = new Random(7722);
 
-        [Params(10, 20, 100, 200, 1000)]
+        [Params(10, 20, 100, 1000)]
         public int BlockSize { get; set; }
 
         [GlobalSetup]
         public void GlobalSetup()
         {
             _data = new byte[BlockSize];
-            rand.NextBytes(_data);
+            _rand.NextBytes(_data);
         }
 
-        [Benchmark]
+        [Benchmark(Baseline = true)]
         public void Murmur64()
         {
             var hash = new MurmurHash3();
             hash.ComputeHash64(_data);
         }
 
-        [Benchmark]
-        public void Wyhash64()
-        {
-            WyHash64.ComputeHash64(_data);
-        }
+        //[Benchmark]
+        //public void Wyhash64()
+        //{
+        //    WyHash64.ComputeHash64(_data);
+        //}
 
         [Benchmark]
         public void XXHash64()
         {
             xxHash64.ComputeHash(_data);
+        }
+
+        [Benchmark]
+        public void FarmHash64()
+        {
+            Farmhash.Sharp.Farmhash.Hash64(_data);
+            
         }
 
     }
